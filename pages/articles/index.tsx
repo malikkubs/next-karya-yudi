8455fcb3ac7bdd26bb6a56e458cc771c4360f41f
yudian nanda <yudiannanwebdev@gmail.com>
2023-09-18 15:48:32 +0700

    jalan build

---
+++ pages/articles/index.tsx
@@ -0,0 +1,16 @@
+import TopMenu from "../../components/TopMenu";
+
+function Ariticles() {
+  return (
+    <TopMenu>
+      <div className="h-[3000px] flex flex-col gap-[200px] justify-between">
+        test
+        <a href="/" target="_blank">
+          cek
+        </a>
+      </div>
+    </TopMenu>
+  );
+}
+
+export default Ariticles;
