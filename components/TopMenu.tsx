ad574cd3ab2ea4a8435687b7baa0174112196b13
yudian nanda <yudiannanwebdev@gmail.com>
2025-10-04 12:47:19 +0700

    fix meta by getservice

--- components/TopMenu.tsx
+++ components/TopMenu.tsx
@@ -26,7 +26,7 @@ function TopMenu({ title, desc, ogtype, image, keywords, children }: any) {
         <meta name="keywords" content={keywords} />
         <meta name="news_keywords" content={keywords} />
         <meta name="googlebot" content="index, follow" />
-        <meta name="author" content={title} />
+        <meta name="author" content={"Yudian Malik"} />
         <meta name="language" content="id" />
         <link
           rel="manifest"
