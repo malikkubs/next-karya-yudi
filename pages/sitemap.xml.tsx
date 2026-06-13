a6a4acf180f17ceb68bbf10e5f661ff7e88b2493
yudian nanda <yudiannanwebdev@gmail.com>
2026-06-13 22:24:18 +0700

    save meta ads dan sitema catch

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -15,6 +15,10 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   const restoration = [];
 
   if (res) {
+    res.setHeader(
+      "Cache-Control",
+      "public, s-maxage=3600, stale-while-revalidate=86400",
+    );
     res.setHeader("Content-Type", "text/xml");
     res.write(`<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
